7d1c04132ffc52309c20f2eeeff39c91e4c250b6
Ben Harris <mail@benharr.is>
2017-09-13 11:22:06 -0400

    init

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="tilde.team unix group">
+    <meta name="author" content="Ben Harris">
+    <title>tilde.team</title>
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#00cc00">
+
+    <link rel="stylesheet" href="/css/hacker.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+
+</head>
+
+<body>
+    <div class="container">
+    <h1>~~~tilde.team~~~</h1>
+    <pre style="background-color:#333;">
+   __  _ __    __      __
+  / /_(_) /___/ /__   / /____  ____ _____ ___
+ / __/ / / __  / _ \ / __/ _ \/ __ `/ __ `__ \
+/ /_/ / / /_/ /  __// /_/  __/ /_/ / / / / / /
+\__/_/_/\__,_/\___(_)__/\___/\__,_/_/ /_/ /_/
+</pre>
+
+    <br>
+    <p>tilde.team is one tiny standard unix computer in the cloud that anyone can use and learn to use in the shared pursuit of cool sites and unix tools.</p>
+
+    <p>i created this site after hearing about paul ford's <a href="http://tilde.club">tilde.club</a>. when i was unable to join due to the waitlist, i decided to create my own. see <a href="/~ben">my tilde page</a> or my <a href="https://benharr.is">personal site</a> for more info about me.</p>
+
+    <br>
+    <a href="/signup" class="btn btn-primary btn-lg"><i class="fa fa-user-plus"></i> signup</a>
+
+    <a href="/faq" class="btn btn-warning btn-lg"><i class="fa fa-info"></i> faq</a>
+
+    <br>
+    <br>
+    <hr>
+
+    <h1>other tilde services</h1>
+
+    <a class="btn btn-info" href="https://cloud.tilde.team"><i class="fa fa-cloud"></i> tilde~cloud</a>
+    <a class="btn btn-info" href="https://bhh.sh"><i class="fa fa-link"></i> url shortener</a>
+    <a class="btn btn-info" href="https://bag.tilde.team"><i class="fa fa-shopping-bag"></i> tilde~wallabag read it later</a>
+    <a class="btn btn-info" href="https://paste.tilde.team"><i class="fa fa-clipboard"></i> tilde~pastebin</a>
+    <a class="btn btn-info" href="https://drop.tilde.team"><i class="fa fa-file"></i> tilde~filedrop</a>
+    <br>
+    <hr>
+
+    <h1>users:</h1>
+        <?php foreach (array_diff(scandir("/home"), ['..', '.']) as $user): ?>
+            <div class="list-group">
+                <a href="/~<?=$user?>" class="list-group-item">
+                    <h4 class="list-group-item-heading">~<?=$user?></h4>
+                </a>
+            </div>
+        <?php endforeach; ?>
+
+    <hr>
+    </div>
+
+<br>
+<br>
+</body>
+</html>
+
+
---
+++ signup/index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="tilde.team unix group">
+    <meta name="author" content="Ben Harris">
+    <title>signup~tilde.team</title>
+
+    <meta name="theme-color" content="#00cc00">
+
+    <script src="/js/vue.min.js"></script>
+    <link rel="stylesheet" href="https://tilde.team/css/hacker.css">
+
+</head>
+
+<body>
+    <div class="container" id="app">
+        <h1>
+            tilde.team signup
+            <div class="pull-right">
+                <small><a href="/">&lt; back</a></small>
+            </div>
+        </h1>
+        <h3>so you wanna join the tilde.team?</h3>
+
+        <p>hi there. fill out this form and i'll get back to you with your account info.</p>
+
+        <hr>
+
+        <form method="post">
+
+            <?php
+            if ($_SERVER["SERVER_NAME"] != "localhost") {
+                require_once "/home/ben/ultimate-email/support/smtp.php";
+            }
+
+            $message = "";
+            if (isset($_REQUEST["username"]) && isset($_REQUEST["email"])) {
+                // Check the name.
+                $name = trim($_REQUEST["username"]);
+                if ($name == "") {
+                    $message .= "<li>please fill in your desired username</li>";
+                }
+                if (strlen($name) > 32) {
+                    $message .= "<li>username too long (32 character max)</li>";
+                }
+                if (preg_match("/([a-z_][a-z0-9_]{0,30})/", $name) == 1) {
+                    echo '';
+                } else {
+                    $message .= "<li>username contains invalid characters (lowercase only)</li>";
+                }
+
+                if (posix_getpwnam($name)) {
+                    $message .= "<li>sorry, the username $name is unavailable</li>";
+                }
+
+                // Check the e-mail address.
+                $email = trim($_REQUEST["email"]);
+                if ($email == "")  {
+                    $message .= "<li>please fill in your email address</li>";
+                } else {
+                    $result = SMTP::MakeValidEmailAddress($_REQUEST["email"]);
+                    if (!$result["success"]) {
+                        $message .= "<li>invalid email address: " . htmlspecialchars($result["error"]) . "</li>";
+                    } elseif ($result["email"] != $email) {
+                        $message .= "<li>invalid email address. did you mean:  " . htmlspecialchars($result["email"]) . "</li>";
+                    }
+                }
+
+                if ($message == "") {
+                    $headers = "From: {$_REQUEST["username"]} <{$_REQUEST["email"]}>\n";
+                    $headers .= "Sender: www-data@tilde.team\n";
+                    $headers .= "To: admin@tilde.team\n";
+                    $headers .= "Reply-To: {$_REQUEST["email"]}\n";
+                    $headers .= "MIME-Version: 1.0\r\n";
+                    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+                    $forwardmail = $_REQUEST["forward_email"] == "on" ? '<a href="https://domains.google.com/registrar#z=e&d=3471834,tilde.team&chp=z,d">yes</a>' : "no";
+
+                    $msgbody = "<h1>New tilde.team signup</h1>
+                                <hr>
+                                Desired username: <strong>{$_REQUEST["username"]}</strong><br>
+                                Contact email: <strong><a href=\"mailto:{$_REQUEST["email"]}\">{$_REQUEST["email"]}</a></strong><br>
+                                Reason: <strong>{$_REQUEST["interest"]}</strong><br>
+                                Forward mail?: <strong>$forwardmail</strong><br>
+                                SSH Key: <pre>{$_REQUEST["sshkey"]}</pre>";
+
+                    if (mail('bharrismac@gmail.com', 'tilde.team signup', $msgbody, $headers)) {
+                       echo '<div class="alert alert-success" role="alert">
+                                email sent! i\'ll get back to you soon with login instructions! <a href="/">back to tilde.team home</a>
+                             </div>';
+                    } else {
+                       echo "The email has failed!";
+                    }
+                } else {
+                    ?>
+                    <div class="alert alert-warning" role="alert">
+                        <strong>please correct the following errors: </strong>
+                        <?=$message?>
+                    </div>
+                    <?php
+                }
+            }
+            ?>
+
+            <div class="form-group">
+                <label>your desired username (numbers and letters only, no spaces)</label>
+                <input v-model="username" class="form-control" name="username" value="<?=$_REQUEST["username"] ?? ""?>" type="text" required>
+            </div>
+
+            <div class="form-group">
+                <label>email to contact you with account info</label>
+                <input v-model="email" class="form-control" name="email" value="<?=$_REQUEST["email"] ?? ""?>" type="text" required>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" <?=isset($_REQUEST["forward_email"]) && $_REQUEST["forward_email"] == "on" ? 'checked="checked"' : ""?> name="forward_email">
+                        forward mail sent to {{ username || 'username' }}@tilde.team to {{ email || 'this address' }}?
+                    </label>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label>what interests you about tilde.team?</label>
+                <textarea class="form-control" name="interest" id="" cols="30" rows="10"><?=$_REQUEST["interest"] ?? ""?></textarea>
+            </div>
+
+            <div class="form-group">
+                <label>SSH public key (optional)</label>
+                <textarea class="form-control" name="sshkey" id="" cols="30" rows="10"><?=$_REQUEST["sshkey"] ?? ""?></textarea>
+            </div>
+
+            <button class="btn btn-primary" type="submit">submit</button>
+
+        </form>
+        <br>
+    </div>
+
+    <script>
+        var vm = new Vue({
+            el: '#app',
+            data: {
+                username: '',
+                email: ''
+            }
+        })
+    </script>
+
+</body>
+</html>
+
+
