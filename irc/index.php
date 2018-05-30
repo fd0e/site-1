9783f4e689a26d5868ff1ac895f3780d52138652
Ben Harris <ben@tilde.team>
2018-05-30 12:11:01 -0400

    update some links

--- irc/index.php
+++ irc/index.php
@@ -8,12 +8,12 @@
 <p>there are a couple places that i'm trying to coordinate for tilde irc:</p>
 
 <ul>
-    <li><a href="http://webchat.freenode.net/?channels=%23%23tilde.team">freenode##tilde.team</a></li>
-    <li><a href="http://webchat.freenode.net/?channels=%23%23tildeverse">freenode##tildeverse</a></li>
     <li><a href="https://irc.tilde.team">irc.tilde.team</a></li>
 </ul>
 
 <p>additionally, i'm running a web client at <a href="https://irc.tilde.team">irc.tilde.team</a> (port 80/443)</p>
+
+<p>connect to irc.tilde.team:6697 with ssl if you want to use your own client!</p>
 
 <p>hit me up on any of these places (or any of the other places you can find on my <a href="/~ben/">tilde page</a>) if you have any suggestions!</p>
 
